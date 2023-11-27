c76c4e92aaf3690a2118b7e8cd6161e659a97161
baegyutae <141345981+baegyutae@users.noreply.github.com>
2023-11-27 23:31:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,19 @@
 class Solution {
     public int solution(int num) {
-        long n = (long) num;
-        int answer = 0;
+        long n = num;
+        int count = 0;
         
         while (n != 1) {
-            if (answer == 500) {
+            if (count == 500) { // 500번 반복해도 1이 되지 않으면 -1 반환
                 return -1;
             }
-            
-            if (n % 2 == 0) {
+            if (n % 2 == 0) { // n이 짝수인 경우
                 n /= 2;
-            } else {
+            } else { // n이 홀수인 경우
                 n = n * 3 + 1;
             }
-            
-            answer++;
+            count++; // 반복 횟수 증가
         }
-        
-        return answer;
+        return count; // 반복 횟수 반환
     }
 }
