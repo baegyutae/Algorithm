e6a6570648b4a190fb99fec74e5e9920b37687cf
baegyutae <141345981+baegyutae@users.noreply.github.com>
2023-12-26 00:36:23 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+
+    public String solution(String myString) {
+        // myString에 포함된 모든 문자를 소문자로 변환
+        String answer = myString.toLowerCase();
+        return answer;
+    }
+}
